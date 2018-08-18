7ec6f8fd9dfcb7cb0cd5ee550db6e8a8898c2fe1
Aric Coady <aric.coady@gmail.com>
2018-08-18 15:28:53 -0700

    Cython source coverage.

--- Makefile
+++ Makefile
@@ -6,6 +6,11 @@ check: all
 	flake8
 	pytest --cov
 
+html: spector/vector.pyx
+	python3 setup.py build_ext -i --define CYTHON_TRACE
+	pytest --cov --cov-report $@
+	cython -a $?
+
 clean:
 	hg st -in | xargs rm
 	rm -rf build dist spector.egg-info
--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     url='https://bitbucket.org/coady/spector',
     license='Apache Software License',
     packages=['spector'],
-    ext_modules=cythonize([ext_module], language='c++'),
+    ext_modules=cythonize([ext_module], language='c++', compiler_directives={'linetrace': True}),
     keywords='sparse array vector matrix numpy scipy',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
@@ -28,9 +28,9 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
 )
